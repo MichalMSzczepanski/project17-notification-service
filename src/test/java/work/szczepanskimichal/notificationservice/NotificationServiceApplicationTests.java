4d098918bfeb5eca12da24ef364ccaa94e5ef935
michals <michal.m.szczepanski@gmail.com>
2024-02-25 19:23:29 +0100

    initial commit

---
+++ src/test/java/work/szczepanskimichal/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package work.szczepanskimichal.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
